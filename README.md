6f9dfb0d35b713815b8bc13ec83beb7eeb9e1b44
Adrien BEHADIR <171994@supinfo.com>
2017-06-18 09:51:33 +0200

    Initialisation

---
+++ README.md
@@ -0,0 +1,6 @@
+it-gbrassens
+
+Ce site est un projet bénévole. Sa conception est inspiré de la charte graphique 
+www.w3schools.com. Initialement développé pour le centre socioculturel Georges Brassens de 
+Bondy, dans le but de fournir un site fonctionnel et simplifié pour la rédaction d'articles 
+des débutants en développement web. Sa réappropriation est disponible librement.
---
+++ rsc/js/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+function open_search(elmnt, trigger) {
+    if (elmnt.css("display") == "none") {
+        elmnt.css("display", "block");
+        trigger.removeClass("fa-search").addClass("fa-close");
+    }
+    else {
+        elmnt.css("display", "none");
+        trigger.removeClass("fa-close").addClass("fa-search");
+    }
+}
+
+function ajax() {
+
+    var pathFile = document.location.pathname;
+    var fileName = pathFile.match(/[^\/]+$/);
+    var pathOnly = pathFile.replace(fileName, '');
+    var pathModule = pathOnly;
+
+    if (pathOnly == "/") {
+        fileName = "index.html";
+        pathOnly = "/";
+        pathFile = pathOnly + fileName;
+        pathModule = "/modules/home/";
+        var pathIndex = null;
+    }
+
+    $('nav').load('/navigation.html', function() {
+        if (pathOnly.includes("home")) {
+            pathOnly = "/";
+        }
+        $("a[href='" + pathOnly + "index.html" + "']").addClass("active");
+    });
+
+    $('aside').load(pathModule + "menu.html", function() {
+        $("a[href='" + pathFile + "']").addClass("active");
+    });
+
+    $('footer').load('/footer.html', function() {
+    });
+}
+
+$(function () {
+    $(".dropdown").has('a.active').addClass('active');
+});
